28a44120e0081e0f10e1fc92c29d78d40588f633
abhi5618 <114580669+abhievergreen5618@users.noreply.github.com>
2023-04-26 17:02:49 +0530

    Update StripeWebhookController.php

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -206,14 +206,15 @@ class StripeWebhookController extends Controller
         // Handle the event
         switch ($event->type) {
             case 'subscription_schedule.canceled':
-            $subscriptionSchedule = $event->data->object;
-            $customer = Customer::retrieve($subscriptionSchedule->customer);
-            $user_id = $customer->metadata->auth0_user_id;
-            $url = "https://".env('AUTH0_DOMAIN')."/api/v2/users/".$user_id;
-            $access_token = get_access_token();
-            $metadata = ['role' => 'free'];
-            $metadata = json_encode($metadata);
-            update_user_meta($access_token,$url,$metadata);
+                $subscriptionSchedule = $event->data->object;
+                $customer = Customer::retrieve($subscriptionSchedule->customer);
+                dd($customer);
+                $user_id = $customer->metadata->auth0_user_id;
+                $url = "https://".env('AUTH0_DOMAIN')."/api/v2/users/".$user_id;
+                $access_token = get_access_token();
+                $metadata = ['role' => 'free'];
+                $metadata = json_encode($metadata);
+                update_user_meta($access_token,$url,$metadata);
             break;
             case 'subscription_schedule.expiring':
                 $subscriptionSchedule = $event->data->object;
